8346ee6449656ea2b56ed01054a940c5b962db0b
chris mott <chrismott@uiowa.edu>
2022-04-22 21:23:06 +0100

    game playing and sending serial monitor correctly. Expects input of 1 when returned. DONE or COMPLETE will not work

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-
+#!/usr/bin/env python3
 import chess
 import time
 import sys
 import dijkstar as djk
 import helper_function as hf
 import Graph
+
+import serial
+import time
+
+# Arduino serial connection
+if __name__ == '__main__':
+ ser = serial.Serial('/dev/ttyACM0', 9600)
+ ser.reset_input_buffer()
 
 def getNodeCoordsStr(node):
     return f'{node % 17},{node // 17}'
@@ -30,6 +38,7 @@ for i in range(10):
 
 
   print(str(legal_moves[1]))
+  
   # populate the graph with new game state
   graph = djk.Graph()
   cleaned_str = hf.clean_board_str(str(board))
@@ -45,12 +54,47 @@ for i in range(10):
   fromNode, tonNode =  hf.UCItoNodeNums(str(legal_moves_ai[1]))
   path = djk.find_path(graph, fromNode, tonNode)
   print(path.nodes)
-  print("move: " + getNodeCoordsStr(path.nodes[0]))
-  print("RAISE MAGNET")
-  for node in path.nodes[1:]:
-      print("move: " + getNodeCoordsStr(node))
 
-  print("LOWER MAGNET")
+  command_bytes = str.encode("M" + getNodeCoordsStr(path.nodes[0]))
+  ser.write(command_bytes + b"\n")
+  print("M" + getNodeCoordsStr(path.nodes[0]))
+  
+  # wait for response from serial comm.
+  tdata = ser.read().decode("utf-8")  
+  data_left = ser.inWaiting()  # Get the number of characters ready to be read
+  tdata += ser.read(data_left).decode("utf-8")
+  print(f'{tdata} received.')
+
+  # raise magnet up to the board
+  command_bytes = str.encode("S1")
+  ser.write(command_bytes + b"\n")
+  print("S1")
+  
+  # wait for response
+  tdata = ser.read().decode("utf-8")  
+  data_left = ser.inWaiting()  # Get the number of characters ready to be read
+  tdata += ser.read(data_left).decode("utf-8")
+  print(f'{tdata} received.')
+
+  # loop through the rest of the moves
+  for node in path.nodes[1:]:
+      print("M" + getNodeCoordsStr(node))
+      command_bytes = str.encode("M" + getNodeCoordsStr(path.nodes[0]))
+      ser.write(command_bytes + b"\n")
+
+      tdata = ser.read().decode("utf-8")  
+      data_left = ser.inWaiting()  # Get the number of characters ready to be read
+      tdata += ser.read(data_left).decode("utf-8")
+      print(f'{tdata} received.')
+
+  print("S0")
+  command_bytes = str.encode("S0")
+  ser.write(command_bytes + b"\n")
+
+  tdata = ser.read().decode("utf-8")  
+  data_left = ser.inWaiting()  # Get the number of characters ready to be read
+  tdata += ser.read(data_left).decode("utf-8")
+  print(f'{tdata} received.')
 
 
   # Generate path of nodes for that movement
